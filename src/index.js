dab2c6f0709b1cd80468cc3d8f720b79c4d336e3
JuliaLC1 <julialussiercraig@gmail.com>
2022-10-15 18:27:10 +0100

    updated location, date and weather info

---
+++ src/index.js
@@ -0,0 +1,42 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hour}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  let temp = document.querySelector("#temp-number");
+  temp.innerHTML = Math.round(response.data.main.temp);
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.name;
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].description;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.main.humidity;
+  let windSpeed = document.querySelector("#wind-speed");
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  let currentDate = document.querySelector("#current-date");
+  currentDate.innerHTML = formatDate(response.data.dt * 1000);
+}
+
+let apiKey = "ac49392e9f3cf46d70ceaaac9615d827";
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+axios.get(apiUrl).then(displayTemperature);
